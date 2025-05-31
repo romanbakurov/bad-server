7c905c174d53010fa03bd09d2d1d7ab8f69b9ec3
romanbakurov <ibakur@ya.ru>
2025-05-31 20:34:12 +0300

    Changed protection-csrf.ts

--- backend/src/middlewares/protection-csrf.ts
+++ backend/src/middlewares/protection-csrf.ts
@@ -1,8 +1,10 @@
 import { doubleCsrf, DoubleCsrfConfigOptions } from 'csrf-csrf'
+import  { Request } from 'express';
 import { CSRF_SECRET, CSRF_COOKIE_NAME } from '../config'
 
 export const doubleCsrfConfigOptions: DoubleCsrfConfigOptions = {
     getSecret: () => CSRF_SECRET,
+    getSessionIdentifier: (req: Request) => req.headers['x-csrf-token']?.toString() || 'anonymous',
     cookieName: CSRF_COOKIE_NAME,
     cookieOptions: {
         sameSite: 'strict',
@@ -13,5 +15,5 @@ export const doubleCsrfConfigOptions: DoubleCsrfConfigOptions = {
     },
 }
 
-export const { doubleCsrfProtection, generateToken } =
+export const { doubleCsrfProtection, invalidCsrfTokenError, generateCsrfToken } =
     doubleCsrf(doubleCsrfConfigOptions)
